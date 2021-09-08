ce359c4bdd65004f898d98e8eb9d5ac20b9e6150
João Gabriel Gonçalves <dev202084@gmail.com>
2021-09-08 17:11:22 -0300

    Primeira versao do site

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Este é um projeto no qual desenvolvi para uma amiga fotógrafa. Neste projeto utilizei apenas html,css e algumas bibliotecas javascript(jquery e magnific popup).
---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+  $('.galeria').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{enabled:true}
+    // other options
+  });
